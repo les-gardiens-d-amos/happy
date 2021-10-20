9e85106f4110e1ab2991f88482e5334cdf6a7fe2
kirua <yo44prg@icloud.com>
2021-10-20 17:38:14 +0200

    feat: add first spec for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+
     def self.find_user_by_name(name)
         User.where(name: name)
     end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @data = {
+      "name" => "plop",
+      "password" => "plop-man"
+    }
+  end
+
+  test "should not save User withouth name" do
+    user = User.new
+    assert_not user.save, "Saved the user without a name"
+  end
+
+  test "save user" do
+    
+    user = User.new(@data)
+    assert user.save, "Saved the user"
+  end
+
+  test "Find user by name" do
+    user = User.new(@data)
+    user.save
+    all_user = User.find_user_by_name("plop")
+    assert_not_equal(0, all_user.length, "Return all user with a name plop")
+  end
 end
