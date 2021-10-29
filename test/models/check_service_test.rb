f99a29c10c7f1398ee7faafc4aa0bc906bf2e857
kirua <yo44prg@icloud.com>
2021-10-29 13:37:21 +0200

    feat: add spec for check service model

--- test/models/check_service_test.rb
+++ test/models/check_service_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class CheckServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should save check service" do
+    check = CheckService.new
+    assert check.save, "Saved the the check service"
+  end
+
+  test "Delete all service" do
+    CheckService.new.save
+    CheckService.delete_all
+    all_check = CheckService.all
+    assert_equal([], all_check, "Return empty array")
+  end
 end
