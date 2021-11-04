3b22cf42f5680248dd7931f3a0d51f57c443cd5a
kirua <yo44prg@icloud.com>
2021-11-04 16:21:44 +0100

    feat: add method for manage error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,35 +1,48 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
+  around_action :manage_error
+  before_action :authorized
 
-    def auth_header
-        request.headers['Authorization']
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, nil, false)
+      rescue JWT::DecodeError
+        nil
       end
-    
-      def decoded_token
-        if auth_header
-          token = auth_header.split(' ')[1]
-          begin
-            JWT.decode(token, nil, false)
-          rescue JWT::DecodeError
-            nil
-          end
-        end
-      end
-    
-      def logged_in_user
-        if decoded_token
-          user_id = decoded_token[0]['user_id']
-          @user = User.find_by(id: user_id)
-        end
-      end
-    
-      def logged_in?
-        !!logged_in_user
-      end
-    
-      def authorized
-        if ENV['RAILS_ENV'] != "test"
-          render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
-        end
-      end
+    end
+  end
+
+  def logged_in_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!logged_in_user
+  end
+
+  def authorized
+    if ENV['RAILS_ENV'] != "test"
+      render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+    end
+  end
+
+  protected
+
+  def manage_error
+    begin
+      yield
+    rescue StandardError => err
+      error_info = { name: request, description: params, error: err, stack_trace: err.backtrace.join("/n") }
+      FailedJob.new(error_info).save
+      raise err
+    end
+  end
 end
