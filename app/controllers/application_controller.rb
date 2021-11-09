88aa94dab2964aabe8bf40684849b4d37c13a5c8
kirua <yo44prg@icloud.com>
2021-11-09 14:03:31 +0100

    fix: rubocop error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::API
   def logged_in_user
     return unless decoded_token
 
-    user_id = decoded_token[0]['user_id']
+    user_id = decoded_token[0]["user_id"]
     @user = User.find_by(id: user_id)
   end
 
