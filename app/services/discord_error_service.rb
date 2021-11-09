c5320751b65a08a03944d488a25ed1522d5cafd6
kirua <yo44prg@icloud.com>
2021-11-09 10:11:07 +0100

    feat: refactor discord error service

--- app/services/discord_error_service.rb
+++ app/services/discord_error_service.rb
@@ -12,31 +12,22 @@ class DiscordErrorService
 
   def send_error_back
     @stack_trace = @stack_trace.slice(0, @stack_trace.length / 6)
-    client = Discordrb::Webhooks::Client.new(url: @url_webhook)
-    client.execute do |builder|
-      builder.content = 'An error has occurred ! ' + random_smiley
-      builder.add_embed do |embed|
-        embed.title = @error.to_s
-        embed.description = "REQUEST : \n \n"
-        embed.description += @request.to_s
-        embed.description = "\n \n PARAMS : \n \n"
-        embed.description += @params.to_s
-        embed.description += "\n \n STACK TRACE : \n \n"
-        embed.description += @stack_trace.to_s
-        embed.timestamp = Time.now
-      end
-    end
+    send_error("REQUEST", "PARAMS")
   end
 
   def send_error_front
+    send_error("DESCRIPTION", "DESCRIPTION")
+  end
+
+  def send_error(request, params)
     client = Discordrb::Webhooks::Client.new(url: @url_webhook)
     client.execute do |builder|
       builder.content = 'An error has occurred ! ' + random_smiley
       builder.add_embed do |embed|
         embed.title = @error.to_s
-        embed.description = "DESCRIPTION : \n \n"
+        embed.description = "#{request} : \n \n"
         embed.description += @request.to_s
-        embed.description = "\n \n DESCRIPTION : \n \n"
+        embed.description = "\n \n #{params} : \n \n"
         embed.description += @params.to_s
         embed.description += "\n \n STACK TRACE : \n \n"
         embed.description += @stack_trace.to_s
