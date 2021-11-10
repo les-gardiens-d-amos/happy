3ef3d81bf7913599e1598ba9d7b114fa73d5ece6
kirua <yo44prg@icloud.com>
2021-11-10 13:42:12 +0100

    feat: update method for find all user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    @users = User.select(:id, :email, :name, :created_at, :connected_at).all
 
     render json: @users
   end
